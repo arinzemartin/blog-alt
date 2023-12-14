d31d640a976620b52f8e7bc6016ddf92049c99da
Martin Arinze <arinzemartin68@gmail.com>
2023-12-14 14:24:39 +0100

    done

---
+++ blog/config/database.py
@@ -0,0 +1,20 @@
+from mysql.connector import connect
+from .variables import DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER
+def get_connection():
+  try:
+    db = connect (
+      host=DB_HOST,
+      port=DB_PORT,
+      user=DB_USER,
+      password=DB_PASSWORD,
+      database=DB_NAME
+    )
+
+    if db.is_connected(): 
+      print("DATABASE CONNECTED")
+
+    cursor = db.cursor(dictionary=True, buffered=True)
+    return db, cursor
+
+  except Exception as e:
+    print("DATABASE ERROR:", str(e))
---
+++ blog/config/variables.py
@@ -0,0 +1,11 @@
+from dotenv import dotenv_values
+
+ENV = dotenv_values()
+
+# ENV VARIABLES
+SECRET_KEY = ENV['SECRET_KEY']
+DB_HOST= ENV["DB_HOST"]
+DB_PORT= ENV["DB_PORT"]
+DB_USER= ENV["DB_USER"]
+DB_PASSWORD= ENV["DB_PASSWORD"]
+DB_NAME= ENV["DB_NAME"]
